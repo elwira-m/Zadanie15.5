7324fc9d1756ad76ec7f2afdd1ec1f998e074521
elwira-m <36117564+elwira-m@users.noreply.github.com>
2018-04-24 03:53:42 +0200

    Add files via upload

---
+++ components/App.js
@@ -0,0 +1,74 @@
+App = React.createClass({
+    getInitialState() {
+    return {
+        loading: false,
+        searchingText: '',
+        gif: {}
+    };
+	},
+	
+	handleSearch: function(searchingText) {  // 1.
+		this.setState({
+			loading: true  // 2.
+		});
+		this.getGif(searchingText, function(gif) {  // 3.
+			this.setState({  // 4
+				loading: false,  // a
+				gif: gif,  // b
+				searchingText: searchingText  // c
+			});
+		}.bind(this));
+    },
+	
+
+	getGif: function(searchingText) {
+
+		return new Promise (
+			let url = 'https://api.giphy.com' + '/v1/gifs/random?api_key=' + 'dc6zaTOxFJmzC' + '&tag=' + searchingText; 
+			
+			function (resolve, reject) {
+			
+				let xhr = new XMLHttpRequest(); 
+				xhr.open('GET', url);
+				xhr.onload = function() {
+					if (xhr.status === 200) {
+						let data = JSON.parse(xhr.responseText).data; 
+						let gif = {
+							url: data.fixed_width_downsampled_url,
+							sourceUrl: data.url
+					};
+					resolve(gif);
+					}else {
+						//reject(console.log('nie udalo sie(:'));
+						reject(new Error(this.statusText))
+					}
+				};
+				xhr.send();
+			}	
+		)
+	},
+	
+	render: function() {
+
+        let styles = {
+            margin: '0 auto',
+            textAlign: 'center',
+            width: '90%'
+        };
+
+        return (
+            <div style={styles}>
+                <h1>Wyszukiwarka GIFow!</h1>
+                <p>Znajdź gifa na <a href='http://giphy.com'>giphy</a>. Naciskaj enter, aby pobrać kolejne gify.</p>
+                <Search onSearch={this.handleSearch}/>
+				<Gif
+					loading={this.state.loading}
+					url={this.state.gif.url}
+					sourceUrl={this.state.gif.sourceUrl}
+				/>
+            </div>
+        );
+    }
+});
+
+
